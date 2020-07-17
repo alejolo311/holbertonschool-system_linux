76477d28b015f4c72ed3c6ef2456b3faaad402d1
alejolo311 <alejolo311@gmail.com>
2020-07-17 18:29:14 -0500

    fix bad parethenses in a condition

--- 0x00-ls/lib.c
+++ 0x00-ls/lib.c
@@ -183,8 +183,8 @@ int print_dir(char **files, char *args, char *folder)
 		flag_1(files, folder, &buffer);
 	if (include(args, 'l'))
 		flag_l(files, folder);
-	if ((_strcmp(args, "-") == 0 || include(args, 'A') ||
-		include(args, 'r') || (include(args, 'a')) && !include(args, 'l') && !include(args, '1')))
+	if ((_strcmp(args, "-") == 0 || include(args, 'r')) ||
+		(include(args, 'a') && include(args, 'A') &&!include(args, 'l') && !include(args, '1')))
 		{
 			without_flags(files, folder, &buffer);	
 		}
