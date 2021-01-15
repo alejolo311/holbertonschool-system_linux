0fb93a6fd22751e25d8aa821b828acb13572444e
alejolo311 <alejolo311@gmail.com>
2021-01-15 02:39:53 -0500

    return value

--- 0x09-strace/lib.c
+++ 0x09-strace/lib.c
@@ -18,6 +18,7 @@ void trace(char **argv, char **env)
 /**
  * tracer -  run tracer
  * @pid: to trace
+ * @method: task
  */
 void tracer(pid_t pid, int method)
 {
@@ -31,14 +32,38 @@ void tracer(pid_t pid, int method)
 		if (_syscallWait(pid) != 0)
 			break;
 		ptrace(PTRACE_GETREGS, pid, 0, &registers);
-        if (method == 1)
-		    printf("%ld\n", (size_t) registers.orig_rax);
-        if (method == 2)
-		    printf("%s", syscalls_64_g[(size_t) registers.orig_rax].name);
+		if (method == 1)
+			printf("%ld\n", (size_t) registers.orig_rax);
+		if (method == 2 || method == 3)
+			printf("%s", syscalls_64_g[(size_t) registers.orig_rax].name);
 		fflush(stdout);
-        method == 2 ? printf("\n") : 0;
+		method == 2 ? printf("\n") : 0;
 		if (_syscallWait(pid) != 0)
 			break;
+	}
+}
+/**
+ * tracer2 -  run tracer
+ * @pid: to trace
+ */
+void tracer2(pid_t pid)
+{
+	int status;
+	struct user_regs_struct regs;
+
+	waitpid(pid, &status, 0);
+	ptrace(PTRACE_SETOPTIONS, pid, 0, PTRACE_O_TRACESYSGOOD);
+	while (1)
+	{
+		if (_syscallWait2(pid) != 0)
+			break;
+		ptrace(PTRACE_GETREGS, pid, 0, &regs);
+		printf("%s", syscalls_64_g[(size_t) regs.orig_rax].name);
+		fflush(stdout);
+		if (_syscallWait2(pid) != 0)
+			break;
+		ptrace(PTRACE_GETREGS, pid, 0, &regs);
+		printf(" = %#lx\n", (unsigned long)regs.rax);
 	}
 }
 
@@ -61,4 +86,25 @@ int _syscallWait(pid_t pid)
 			return (1);
 	}
 }
+/**
+ * _syscallWait2 -  trap syscall
+ * @pid: pid to trace
+ * Return: 1 if sycall was captured
+ */
+int _syscallWait2(pid_t pid)
+{
+	int status;
 
+	while (1)
+	{
+		ptrace(PTRACE_SYSCALL, pid, 0, 0);
+		waitpid(pid, &status, 0);
+		if (WIFSTOPPED(status) && WSTOPSIG(status) & 0x80)
+			return (0);
+		if (WIFEXITED(status))
+		{
+			printf(" = ?\n");
+			return (1);
+		}
+	}
+}
--- 0x09-strace/strace.h
+++ 0x09-strace/strace.h
@@ -17,9 +17,15 @@
 
 #include "syscalls.h"
 
+typedef int bool;
+#define true 1
+#define false 0
+
 
 void trace(char **argv, char **env);
 int _syscallWait(pid_t child_pid);
 void tracer(pid_t pid, int method);
+int _syscallWait2(pid_t child_pid);
+void tracer2(pid_t pid);
 
 #endif /* strace */
